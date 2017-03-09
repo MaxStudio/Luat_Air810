72530390e47102b49f7ac65c2ed8ffe7d6f20336
flymargin <35525490@qq.com>
2017-03-09 17:08:17 +0800

    add a demo module,'uart'.

---
+++ script/demo/uart/main.lua
@@ -0,0 +1,10 @@
+PROJECT = "uart demo"
+VERSION = "1.0.0"
+
+require"sys"
+require"common" --test模块用到了common.binstohexs接口
+require"pm" --test模块用到了pm.wake接口
+require"test"
+
+sys.init(0,0)
+sys.run()
