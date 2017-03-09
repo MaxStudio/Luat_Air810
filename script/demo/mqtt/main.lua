2910dea5d214f45e3875f55d49c17e8f6444d72e
flymargin <35525490@qq.com>
2017-03-09 16:04:45 +0800

    add a demo module,'mqtt'.

---
+++ script/demo/mqtt/main.lua
@@ -0,0 +1,14 @@
+PROJECT = "mqtt-deom"
+VERSION = "1.0.0"
+
+require"sys"
+require"misc"
+require"link"
+require"linkapp"
+require"common"
+require"mqtt"
+require"test"
+require"dbg"
+
+sys.init(0,0)
+sys.run()
