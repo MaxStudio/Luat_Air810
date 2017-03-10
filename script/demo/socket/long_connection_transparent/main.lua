44278ea4304c022c3d97e9a8ca2dd3d9b6ea1243
flymargin <35525490@qq.com>
2017-03-10 13:14:30 +0800

    add a demo module,'long_connection_transparent',in 'socket'.

---
+++ script/demo/socket/long_connection_transparent/main.lua
@@ -0,0 +1,13 @@
+PROJECT = "long_connection_transparent-demo"
+VERSION = "1.0.0"
+
+require"sys"
+require"misc"
+require"mcuart"
+require"link"
+require"linkapp"
+require"dbg"
+require"sck"
+
+sys.init(0,0)
+sys.run()
