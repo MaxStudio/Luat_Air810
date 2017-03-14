0dfaea8b93e7960dbdd195d0958db3b516335570
flymargin <35525490@qq.com>
2017-03-14 15:49:49 +0800

    Don't require para's file in 'nvm'.

--- script/lib/nvm.lua
+++ script/lib/nvm.lua
@@ -112,7 +112,7 @@ local function load()
 	end
 	f:close()
 	
-	f,para = pcall(require,string.match(paraname,"(/(.+)%.lua)"))
+	f,para = pcall(require,string.match(paraname,"/(.+)%.lua"))
 	if not f then
 		restore()
 		return
