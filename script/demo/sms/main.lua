b5ed7e1e21ad6e6dceeaf2a979091a0d8f2b4789
flymargin <35525490@qq.com>
2017-03-15 10:49:26 +0800

    modify 'sms' demo-module according to new lib.

--- script/demo/sms/main.lua
+++ script/demo/sms/main.lua
@@ -2,9 +2,7 @@ PROJECT = "sms demo"
 VERSION = "1.0.0"
 
 require"sys"
-require"common" --smsapp模块用到了common模块的接口
-require"sms" --smsapp模块用到了sms模块的接口
-require"smsapp"
+require"test"
 
 sys.init(0,0)
 sys.run()
