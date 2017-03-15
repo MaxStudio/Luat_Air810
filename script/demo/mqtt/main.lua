5b1bc085874b9bf271d719b7676a64d7af150dd5
flymargin <35525490@qq.com>
2017-03-15 10:22:26 +0800

    modify 'mqtt' demo-module according to new lib.

--- script/demo/mqtt/main.lua
+++ script/demo/mqtt/main.lua
@@ -1,14 +1,8 @@
-PROJECT = "mqtt-deom"
+PROJECT = "mqtt demo"
 VERSION = "1.0.0"
 
 require"sys"
-require"misc"
-require"link"
-require"linkapp"
-require"common"
-require"mqtt"
 require"test"
-require"dbg"
 
 sys.init(0,0)
 sys.run()
