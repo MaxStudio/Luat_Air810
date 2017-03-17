97f84ae2729ef3247d648af6c0b6f25c7225066b
flymargin <35525490@qq.com>
2017-03-17 15:47:58 +0800

    modify 'gps' demo-module to fix init's error.

--- script/demo/gps/test.lua
+++ script/demo/gps/test.lua
@@ -32,28 +32,30 @@ local function test3cb(cause)
 	print("test3cb",cause,gps.isfix())
 end
 
---UART2外接UBLOX GPS模块
-gps.init(nil,nil,true,1000,2,9600,8,uart.PAR_NONE,uart.STOP_1)
-
 --测试代码开关，取值1,2
 local testidx = 1
 
---第1种测试代码
-if testidx==1 then
-	--执行完下面三行代码后，GPS就会一直开启，永远不会关闭
-	--因为gps.open(gps.DEFAULT,{cause="TEST1",cb=test1cb})，这个开启，没有调用gps.close关闭
-	gps.open(gps.DEFAULT,{cause="TEST1",cb=test1cb})
-	
-	--10秒内，如果gps定位成功，会立即调用test2cb，然后自动关闭这个“GPS应用”
-	--10秒时间到，没有定位成功，会立即调用test2cb，然后自动关闭这个“GPS应用”
-	gps.open(gps.TIMERORSUC,{cause="TEST2",val=10,cb=test2cb})
-	
-	--300秒时间到，会立即调用test3cb，然后自动关闭这个“GPS应用”
-	gps.open(gps.TIMER,{cause="TEST3",val=300,cb=test3cb})
---第2种测试代码
-elseif testidx==2 then
-	gps.open(gps.DEFAULT,{cause="TEST1",cb=test1cb})
-	sys.timer_start(gps.close,30000,gps.DEFAULT,{cause="TEST1"})
-	gps.open(gps.TIMERORSUC,{cause="TEST2",val=10,cb=test2cb})
-	gps.open(gps.TIMER,{cause="TEST3",val=60,cb=test3cb})	
+local function gps_open(typ)
+  --第1种测试代码
+  if typ==1 then
+  	--执行完下面三行代码后，GPS就会一直开启，永远不会关闭
+  	--因为gps.open(gps.DEFAULT,{cause="TEST1",cb=test1cb})，这个开启，没有调用gps.close关闭
+  	gps.open(gps.DEFAULT,{cause="TEST1",cb=test1cb})
+  	
+  	--10秒内，如果gps定位成功，会立即调用test2cb，然后自动关闭这个“GPS应用”
+  	--10秒时间到，没有定位成功，会立即调用test2cb，然后自动关闭这个“GPS应用”
+  	gps.open(gps.TIMERORSUC,{cause="TEST2",val=10,cb=test2cb})
+  	
+  	--300秒时间到，会立即调用test3cb，然后自动关闭这个“GPS应用”
+  	gps.open(gps.TIMER,{cause="TEST3",val=300,cb=test3cb})
+  --第2种测试代码
+  elseif typ==2 then
+  	gps.open(gps.DEFAULT,{cause="TEST1",cb=test1cb})
+  	sys.timer_start(gps.close,30000,gps.DEFAULT,{cause="TEST1"})
+  	gps.open(gps.TIMERORSUC,{cause="TEST2",val=10,cb=test2cb})
+  	gps.open(gps.TIMER,{cause="TEST3",val=60,cb=test3cb})	
+  end
 end
+
+gps.init()
+sys.timer_start(gps_open, 10000, testidx)
