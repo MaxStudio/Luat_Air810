dbb2f465a252d9bb606c11cea53a0dcad7638d36
flymargin <35525490@qq.com>
2017-03-28 15:27:26 +0800

    add 'keypad' lib-module.

---
+++ script/lib/keypad.lua
@@ -0,0 +1,67 @@
+module(...,package.seeall)
+
+local curkey
+local PWROFF_KEY_LONG_PRESS_TIME = 3000
+local keymap = {["255255"] = "K_RED"}
+local sta,keyname = "IDLE"
+
+local function print(...)
+	_G.print("keypad",...)
+end
+
+local function pwoff()
+	print("pwoff")
+	sys.poweroff()
+end
+
+local function repwron()
+  print("repwron")
+  sys.repwron()
+end
+
+local function keylongpresstimerfun()
+	print("keylongpresstimerfun curkey",curkey,keyname,sys.isPwronCharger())
+  if keyname == "K_RED" then
+		if sys.isPwronCharger() then
+		  repwron()
+		else
+      sys.timer_start(pwoff,1000)
+		end
+	end
+	sta = "LONG"
+end
+
+local function stopkeylongpress()
+	curkey = nil
+	sys.timer_stop(keylongpresstimerfun)
+end
+
+local function startkeylongpress(key)
+	print("startkeylongpress",curkey,key,keyname)
+	stopkeylongpress()
+	curkey = key
+	
+	sys.timer_start(keylongpresstimerfun,PWROFF_KEY_LONG_PRESS_TIME)
+end
+
+local function keymsg(msg)
+	print("keypad.keymsg",msg.key_matrix_row,msg.key_matrix_col)
+	local key = keymap[msg.key_matrix_row..msg.key_matrix_col]
+	print("keymsg key",key,msg.pressed,keyname)
+	if key then
+		if msg.pressed then
+			sta = "PRESSED"
+			if not keyname then
+				keyname = key
+			end
+			startkeylongpress(key)			
+		else
+			stopkeylongpress()
+			sta = "IDLE"
+			keyname = nil
+		end
+	end
+end
+
+sys.regmsg(rtos.MSG_KEYPAD,keymsg)
+rtos.init_module(rtos.MOD_KEYPAD,0,0,0)
