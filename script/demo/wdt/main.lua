0a335c82f865e6fc4789942d28f84d8ab6553436
panjun <panjun@airm2m.com>
2017-05-09 19:29:45 +0800

    change watchdog's configuration.

--- script/demo/wdt/main.lua
+++ script/demo/wdt/main.lua
@@ -4,7 +4,7 @@ require"sys"
 require"pins"
 --加载硬件看门狗功能模块
 require"wdt"
-wdt.open({pin=pio.P1_2,dir=pio.INPUT,valid=1},{pin=pio.P0_20,defval=true,valid=1})
+wdt.open({pin=pio.P0_16,dir=pio.INPUT,valid=1},{pin=pio.P0_20,defval=true,valid=1})
 
 sys.init(0,0)
 sys.run()
