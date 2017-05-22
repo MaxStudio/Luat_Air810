76334cd3cb134fe52805c28f8cad5dad180e4e56
panjun <panjun@airm2m.com>
2017-05-22 09:59:31 +0800

    add some comments.

--- script/demo/luatyun/main.lua
+++ script/demo/luatyun/main.lua
@@ -9,6 +9,7 @@ VERSION = "1.0.0"
 2、如果没有用户名，注册用户
 3、注册用户之后，如果没有对应的项目，创建一个新项目
 4、进入对应的项目，点击左边的项目信息，右边会出现信息内容，找到ProductKey：把ProductKey的内容，赋值给PRODUCT_KEY变量
+5、参照script\demo\write_sn写入正确的SN号
 ]]
 PRODUCT_KEY = "v32xEAKsGTIEQxtqgwCldp5aPlcnPs3K"
 require"sys"
